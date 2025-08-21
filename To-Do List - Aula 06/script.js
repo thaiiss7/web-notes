f351c11c77e77d014bc72b12741dba2590ffc761
thaiis7 <thaismichel7@gmail.com>
2025-08-21 11:40:05 -0300

    add to-do list

---
+++ To-Do List - Aula 06/script.js
@@ -0,0 +1,15 @@
+const submit = () => {
+    let input = document.getElementById('texto').value
+    let tarefa = document.getElementById('tarefa')
+    let itens = document.createElement('div')
+    itens.className = 'itens'
+
+    itens.innerHTML = `<p onclick="cross()">${input}</p>
+    <input type="button" value="x">`
+
+    tarefa.appendChild(itens)
+}
+
+const cross = () => {
+
+}
