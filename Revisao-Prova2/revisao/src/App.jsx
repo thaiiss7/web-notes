2bdc356dae7afabb80c078deeabdf6c5de4b8ce1
thaiiss7 <thaismichel7@gmail.com>
2025-10-09 11:13:52 -0300

    organized pastes and add revisao

---
+++ Revisao-Prova2/revisao/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState(0)
+  const [text, setText] = useState("")
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    console.log("testando aqui uai")
+  }, [text])
+
+  return (
+    <>
+
+      <button onClick={() => setValue(value - 1)}>-</button>
+      {value}
+      <button onClick={() => setValue(value + 1)}>+</button>
+
+      <br />
+
+      <input type="text" onChange={ (e) => setText(e.target.value) }/>
+      <h1>{text}</h1>
+
+      <br />
+
+      <button onClick={() => setShow(!show)}>clica vai</button> {/*altera a visibilidade mudando o valor da variável */}
+      { show &&
+      <div style={{backgroundColor: 'blue', width: '200px', height: '200px'}}>você é</div>
+      }
+
+      <br />
+      {/* com ternário */}
+      { show ? <div style={{backgroundColor: 'blue', width: '200px', height: '200px'}}>esquisito</div> : ""}
+    </>
+  )
+}
+
+export default App
